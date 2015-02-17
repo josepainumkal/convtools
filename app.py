650813dfccb2fa955f9ead3453abb4877aaf8ac6
Matthew Turner <maturner01@gmail.com>
2015-02-17 13:17:19 -0800

    added User model and Login view; need now for dynamic user creation

--- app.py
+++ app.py
@@ -5,51 +5,61 @@ file: adaptors/human/app.py
 
 So far only search is exposed. Done via static/search.js.
 """
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template, session, url_for
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField  # , DateTimeField
-# from wtforms.validators import Required
+from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required
 from wcwave_adaptors.watershed import default_vw_client
 
 from collections import defaultdict
 import os
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 key = os.getenv('VWPLATFORM_KEY')
 app.secret_key = os.getenv('VWPLATFORM_KEY')
 
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(BASEDIR, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+manager = Manager(app)
 
 vw_client = default_vw_client()
 
 
+
 #### INDEX ####
 @app.route('/')
-def hello():
+def index():
     "Splash page"
+    if 'user_name' not in session:
+        session['user_name'] = None
+
     # search URL is like https://129.24.196.28
     watershed_ip = vw_client.searchUrl.split('/')[2]
 
-    return render_template("index.html", ip=watershed_ip)
+    return render_template("index.html", ip=watershed_ip,
+                           user_name=session['user_name'])
+
 
 
 #### ABOUT ####
 @app.route('/about')
 def about():
     "About page"
-    return render_template("about.html")
+    return render_template("about.html", user_name=session['user_name'])
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    "Search the virtual watershed and display results"
-
-    # If there are no arguments passed, provide a form for searching
-
-    return make_model_run_panels()
-
-
-def make_model_run_panels():
     """
     Create model run panels, rectangles on the search/home page that display
     summary information about the data that the VW has for a particular model
@@ -80,7 +90,21 @@ def make_model_run_panels():
     panels = {p['model_run_uuid']: p for p in panels}.values()
 
     # pass the list of parsed records to the template to generate results page
-    return render_template('search.html', form=form, panels=panels)
+    return render_template('search.html', form=form, panels=panels,
+                           user_name=session['user_name'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    print form.validate_on_submit()
+
+    if form.user_name.data and form.password.data:
+        session['user_name'] = form.user_name.data
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
 
 
 def _make_panel(search_record):
@@ -119,6 +143,18 @@ def _make_panel(search_record):
     return panel
 
 
+###############
+#### Forms ####
+###############
+class LoginForm(Form):
+    """
+    Login form for /login route
+    """
+    user_name = StringField('Enter your User Name', validators=[Required()])
+    password = PasswordField('Enter your Password', validators=[Required()])
+    submit = SubmitField('Go!')
+
+
 class SearchForm(Form):
     """
     Flask-WTF form for the search page
@@ -129,9 +165,28 @@ class SearchForm(Form):
     submit = SubmitField('Submit')
 
 
+###########################
+#### Model Definitions ####
+###########################
+class User(db.Model):
+    """
+    Our User model. Users have biographical information, a user id, user name,
+    and password.
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    affiliation = db.Column(db.String(64), index=True)
+    state = db.Column(db.String(2), index=True)
+    city = db.Column(db.String(20))
+    email = db.Column(db.String(20))
+    user_name = db.Column(db.String(20), unique=True)
+    # passwd = db.Column(db.String(20), primary_key=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_name
+
 
 if __name__ == "__main__":
-    # app.run()
+    # manager.run()
     app.run(debug=True)
-
-
