7e63b2e6df2e3f82a2e0ce78f3bd7717e6445452
Matthew Turner <maturner01@gmail.com>
2015-02-28 02:55:27 -0800

    successfully adding new users and confirming accounts via email

--- config.py
+++ config.py
@@ -9,10 +9,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('VW_SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('VW_MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('VW_MAIL_PASSWORD')
+
     VWPLATFORM_MAIL_SUBJECT_PREFIX = '[VWPLATFORM]'
     VWPLATFORM_MAIL_SENDER = 'Matthew Turner \
         <vwplatform-admin@northwestknowledge.net'
-    VWPLATFORM_ADMIN = os.environ.get('VWPLATFORM_ADMIN')
+    VWPLATFORM_ADMIN = os.environ.get('VWPLATFORM_ADMIN') or 'Admin'
 
     @staticmethod
     def init_app(app):
