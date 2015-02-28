adb2a0ba49152e9aef785c4f5eded0375a2bf310
Matthew Turner <maturner01@gmail.com>
2015-02-28 02:55:27 -0800

    successfully adding new users and confirming accounts via email

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
+from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from . import db
-
-from . import login_manager
+from . import db, login_manager
 
 
 @login_manager.user_loader
@@ -17,12 +17,16 @@ class User(db.Model, UserMixin):
     and password.
     """
     __tablename__ = 'users'
+
+    # columns with no special methods
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     affiliation = db.Column(db.String(64), index=True)
     state = db.Column(db.String(2), index=True)
     city = db.Column(db.String(20), index=True)
     email = db.Column(db.String(20), unique=True)
+
+    # password/password_hash
     password_hash = db.Column(db.String(128))
 
     @property
@@ -35,6 +39,30 @@ class User(db.Model, UserMixin):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # account confirmation
+
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.secret_key, expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.secret_key)
+        try:
+            data = s.loads(token)
+        except:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+
+        self.confirmed = True
+        db.session.add(self)
+        db.session.commit()
+
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.name
