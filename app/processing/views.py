2f1bafa68e816ee63806e394578290f00ab9f305
lisapalathingal <lisa.palathingal@gmail.com>
2015-05-12 10:16:47 -0700

    updated prms.html

--- app/processing/views.py
+++ app/processing/views.py
@@ -1,7 +1,6 @@
-from flask import render_template
+from flask import request, render_template
 
 from . import processing
-
 
 @processing.route('/', methods=['GET'])
 def processing_index():
@@ -10,3 +9,17 @@ def processing_index():
 @processing.route('/prms', methods=['GET', 'POST'])
 def prms():
     return render_template('processing/prms.html')
+
+@processing.route('/upload', methods=['POST'])
+def upload():
+    
+    if request.files['input-file'].filename == '' or request.files['hru-file'].filename == '' or request.form['row'] == '' or request.form['column'] == '' or request.form['epsg'] == '':
+        return render_template('processing/prms.html', InputErrorMessage = "Please upload required files and/or fill in all the fields")
+
+    numberOfRows = int(request.form['row'])
+    numberOfColumns = int(request.form['column'])
+    epsgValue = int(request.form['epsg'])
+    inputFile = request.files['input-file']
+    hruFile = request.files['hru-file']
+
+    return "YES"
