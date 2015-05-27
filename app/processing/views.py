0a2e70b3a3462fce82756a9dc6dee6def6843e3f
Matthew Turner <maturner01@gmail.com>
2015-05-26 23:13:51 -0700

    workflow to grid making form. need call to JJ app and login.required

--- app/processing/views.py
+++ app/processing/views.py
@@ -7,6 +7,12 @@ from . import processing
 def processing_index():
     return render_template('processing/index.html')
 
+
 @processing.route('/prms', methods=['GET', 'POST'])
 def prms():
     return render_template('processing/prms.html')
+
+@processing.route('/makegrid', methods=['GET', 'POST'])
+def makegrid():
+    return render_template('processing/makegrid.html')
+
