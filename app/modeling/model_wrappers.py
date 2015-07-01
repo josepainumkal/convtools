b8c1ecb8cb57aba9ff9bfd4e094a5d4f5ebdd525
Matthew Turner <maturner01@gmail.com>
2015-06-30 22:54:30 -0700

    tests and vw_isnobal written, need to run tests and get em green

--- app/modeling/model_wrappers.py
+++ app/modeling/model_wrappers.py
@@ -1,15 +1,47 @@
 """
-Harness functions for models. VWHarness Object? Build the Python implementation
-in class form and then abstract to an ontology.
+Wrappers for running models. Functions are like `vw_{model}`
 """
+import os
+
+from wcwave_adaptors import default_vw_client, isnobal, metadata_from_file
 
 
-class ModelWrapper(object):
+def vw_isnobal(input_modelrun_uuid):
     """
-    Model of the modeling workflow. Checks if the ModelHarness is properly
-    configured before running harnessed model.
+    Run isnobal with data from the virtual watershed
     """
-    def __init__(self, arg):
-        super(ModelWrapper, self).__init__()
-        self.arg = arg
+    vwc = default_vw_client()
 
+    # TODO when vwp gets fixed, just use (dataset) uuid
+    input_records = \
+        vwc.dataset_search(model_run_uuid=input_modelrun_uuid,
+                           model_set="inputs")
+
+    assert input_records.total == 1, \
+        "Current implementation of vw_isnobal requires the user\n" + \
+        "to use as input a model_run_uuid pointing to a model run\n" + \
+        "with a single record that is an input"
+
+    try:
+        dl_url = input_records[0]['downloads'][0]['nc']
+    except KeyError as e:
+        e.args = ("downloads or nc not found in isnobal input record")
+        raise
+
+    if not os.path.exists('tmp/'):
+        os.mkdir('tmp/')
+
+    writedir = 'tmp/' + input_modelrun_uuid
+    os.mkdir(writedir)
+    outfile = os.path.join(writedir, 'in.nc')
+
+    input_nc = vwc.download(dl_url, outfile)
+
+    isnobal(input_nc, outfile)
+
+    vwc.upload(input_modelrun_uuid, outfile)
+
+    md = metadata_from_file(outfile)
+    vwc.insert_metadata(md)
+
+    os.removedirs(writedir)
--- test/test_model_wrappers.py
+++ test/test_model_wrappers.py
@@ -3,6 +3,68 @@ Tests for the modeling interface of the virtual watershed platform
 """
 import unittest
 
-from app.modeling.model_wrappers import isnobal_harness
+from xray import open_dataset
+
+from wcwave_adaptors import default_vw_client, metadata_from_file
+
+from app.modeling.model_wrappers import vw_isnobal
+
+# from app.modeling.model_wrappers import isnobal_harness
 
 
+class IsnobalWrapperTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        # netcdfs we'll use for input and check output against
+        nc_isnobal_input = 'test/data/isnobal_input.nc'
+        nc_isnobal_output = 'test/data/isnobal_output.nc'
+
+        # connect to the virtual watershed
+        self.vwc = default_vw_client()
+
+
+        # load NetCDF inputs and outputs from test data
+        self.input_dataset = open_dataset(nc_isnobal_input, 'r')
+        self.output_dataset = open_dataset(nc_isnobal_output, 'r')
+
+        # insert NetCDF test input to virtual watershed
+        input_mr_name = 'webapp-testing-input'
+
+        model_run_uuid = self.input_modelrun_uuid = \
+            vwc.initialize_modelrun(
+                model_run_name=input_mr_name,
+                description='test in vwplatform',
+                researcher_name='Matt Turner',
+                keywords='test,isnobal,webapp')
+
+        vwc.upload(model_run_uuid, nc_isnobal_input)
+
+        md = metadata_from_file(nc_isnobal_input)
+        self.input_uuid = vwc.insert_metadata(md)
+
+    def test_isnobal(self):
+        """Test iSNOBAL wrapper is working properly"""
+
+        # vw_isnobal gives the user the uuid of the file
+        vw_isnobal(self.input_uuid)
+
+        # download output file
+        output_records = \
+            self.vwc.dataset_search(model_run_uuid=self.model_run_uuid,
+                                    model_set="outputs").records
+
+        assert len(output_records) == 1, \
+            "more than one output record for isnobal test"
+
+        dl_url = output_records[0]['downloads'][0]['nc']
+
+        self.vwc.download(dl_url, 'test/data/nc_out_fromvw.tmp')
+
+        # compare output file from VW to expected
+        dataset_from_vw = open_dataset('test/data/nc_out_fromvw.tmp')
+
+        assert dataset_from_vw.variables.keys() == self.output_dataset,\
+            "datasets don't have the same keys"
+
+        assert dataset_from_vw.identical(self.output_dataset)
