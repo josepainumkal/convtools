f5c5a15d5361f204a195ad08aa6605222f685b06
Matthew Turner <maturner01@gmail.com>
2015-09-30 17:32:05 -0700

    removed prms from modeling, ready for new version of online prms

--- app/modeling/views.py
+++ app/modeling/views.py
@@ -1,23 +1,18 @@
 from flask import request, render_template, jsonify
 from flask import current_app as app
-from flask_login import login_required, current_user
-from werkzeug import secure_filename
+from flask_login import login_required
 
 import datetime
 import os
 import random
 import string
-import util
 
 from . import modeling
 from .model_wrappers import vw_isnobal
-from .prms_util import (copyParameterSectionFromInputFile, readncFile,
-                        readtifFile)
 from .gridtoolwrap import end_2_end
 from ..main.views import _make_panel
 
 from wcwave_adaptors import default_vw_client
-from wcwave_adaptors import make_fgdc_metadata, metadata_from_file
 
 
 VW_CLIENT = default_vw_client()
@@ -90,19 +85,6 @@ ISUGT_VARNAME_LOOKUP = {
 }
 
 
-@modeling.route('/prms', methods=['GET', 'POST'])
-@login_required
-def prms():
-
-    # create a new model_run_name
-    name = id_generator()
-
-    # create a new model_run_uuid
-    new_mr_uuid = VW_CLIENT.initialize_modelrun(model_run_name=name, description='lehman creek', researcher_name=current_user.name, keywords='prms,example,nevada')
-
-    return render_template('modeling/prms.html', model_run_uuid=new_mr_uuid)
-
-
 @modeling.route('/isnobal/<model_run_uuid>', methods=['GET', 'POST'])
 def select_isnobal_input(model_run_uuid):
 
@@ -157,71 +139,3 @@ def isnobal():
     return render_template('modeling/isnobal.html',
                            file_name=file_name,
                            panels=panels)
-
-
-@modeling.route('/upload', methods=['POST'])
-def upload():
-
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-    if request.files['input-file'].filename == '' or request.files['hru-file'].filename == '' or request.form['row'] == '' or request.form['column'] == '' or request.form['epsg'] == '':
-        return render_template('modeling/prms.html', InputErrorMessage = "Please upload required files and/or fill in all the fields")
-
-    numberOfRows = int(request.form['row'])
-    numberOfColumns = int(request.form['column'])
-    epsgValue = int(request.form['epsg'])
-    inputFile = request.files['input-file']
-    hruFile = request.files['hru-file']
-    new_mr_uuid = str(request.form['uuid'])
-
-    if inputFile:
-        inputFileName = secure_filename(inputFile.filename)
-        inputFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputFileName))
-
-    if hruFile:
-        hruFileName = secure_filename(hruFile.filename)
-        hruFile.save(os.path.join(app.config['UPLOAD_FOLDER'], hruFileName))
-
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], hruFileName)) as f:
-        numberOfLines = len(f.readlines())
-
-    product = numberOfRows * numberOfColumns
-
-    if product == numberOfLines:
-        hruFileHandle = open(os.path.join(app.config['UPLOAD_FOLDER'], hruFileName), 'r')
-        values = util.findAverageResolution(hruFileHandle, numberOfRows, numberOfColumns)
-
-        inputFileHandle = open(os.path.join(app.config['UPLOAD_FOLDER'], inputFileName), 'r')
-        copyParameterSectionFromInputFile(inputFileHandle)
-        readncFile(numberOfRows, numberOfColumns, epsgValue, values[0], values[1], values[2], values[3])
-        readtifFile(numberOfRows, numberOfColumns, epsgValue, values[0], values[1], values[2], values[3])
-        util.generateMetaData()
-        util.moveFilesToANewDirectory()
-
-        dirList = os.listdir(app.config['DOWNLOAD_FOLDER'])
-        for fname in dirList:
-            print "uploading " + fname
-            res = VW_CLIENT.upload(new_mr_uuid, os.path.join(app.config['DOWNLOAD_FOLDER'], fname))
-
-            input_file = fname
-            parent_uuid = new_mr_uuid
-            description = 'Lehman Creek PRMS Data'
-	    watershed_name = 'Lehman Creek'
-	    state = 'Nevada'
-	    start_datetime = '2010-01-01 00:00:00'
-	    end_datetime = '2010-01-01 01:00:00'
-	    model_name = 'prms'
-
-	    # create XML FGDC-standard metadata that gets included in VW metadata
-	    fgdc_metadata = make_fgdc_metadata(input_file, None, new_mr_uuid, start_datetime, end_datetime, model=model_name)
-
-	    # create VW metadata
-	    watershed_metadata = metadata_from_file(input_file, parent_uuid, new_mr_uuid, description, watershed_name, state, start_datetime=start_datetime, end_datetime=end_datetime, model_name=model_name, fgdc_metadata=fgdc_metadata)
-
-            response = VW_CLIENT.insert_metadata(watershed_metadata)
-
-        return render_template("modeling/prms.html", Success_Message = "Successfully inserted NetCDF and GeoTIFF files in Virtual Watershed")
-    else:
-        return render_template("modeling/prms.html", Error_Message = "The product of the number of rows and columns do not match the number of parameter values")
-
