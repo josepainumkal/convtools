0c6ccc8f2e2f04b381f715e4043a19c1d0de69c2
Matthew Turner <maturner01@gmail.com>
2015-11-05 17:21:25 -0800

    first prototype of file uploads in share/files

--- app/static/js/angular/controllers.js
+++ app/static/js/angular/controllers.js
@@ -1,10 +1,32 @@
-var datasetShareApp = angular.module('datasetShareApp', []);
+var datasetShareApp = angular.module('datasetShareApp', ['ngFileUpload']);
 
-datasetShareApp.controller('DatasetShareCtrl', ['$scope', '$log', 
-  function ($scope, $log) {
+datasetShareApp.controller('DatasetShareCtrl', ['$scope', '$log', 'Upload', '$timeout',
+  function ($scope, $log, Upload, $timeout) {
     $scope.modelRunName = "Yo mama";
     $scope.modelRunDescription = "Some good dro data";
     $scope.modelRunUUID = mrUUID;
 
-    $log.log("uuid: " + mrUUID); // $scope.modelRunUUID);
+    $scope.uploadPic = function(file) 
+    {
+      file.upload = Upload.upload({
+          url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+          data: {
+            file: file, username: $scope.username
+          }
+      });
+      
+      file.upload.then(function (response) {
+        $timeout(function () {
+          file.result = response.data;
+          });  
+        }, function (response) {
+          if (response.status > 0)
+          {
+            $scope.errorMsg = response.status + ': ' + response.data;
+          }
+        }, function(evt) {
+          // Math.min is IE hack to prevent 200% from being displayed
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+    };
 }]);
--- app/static/js/angular/test/unit/controllerSpec.js
+++ app/static/js/angular/test/unit/controllerSpec.js
@@ -1,8 +1,10 @@
 'use strict';
 
 describe('unittests for controller frontend', function () {
+
   it('uploads data and inserts metadata to the virtual watershed', function () {
     expect(false).toBe(true);
   });
+
 });
 
