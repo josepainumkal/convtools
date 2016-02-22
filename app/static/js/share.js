9f54657c669811c8e8389952f851430cce90da67
Matthew Turner <maturner01@gmail.com>
2016-02-22 12:28:11 -0800

    ugly files list working using react

--- app/static/js/share.js
+++ app/static/js/share.js
@@ -1,8 +1,12 @@
+/**
+ * Box that contains the file list
+ */
 var FileBox = React.createClass({
 
     loadFilesFromServer: function() {
         $.ajax({
-            url: '/api/modelruns/c79b1d3a-4ddf-4d19-8ed6-068a86d2de7d/files',
+            //url: '/api/modelruns/c79b1d3a-4ddf-4d19-8ed6-068a86d2de7d/files',
+            url: this.props.baseUrl + this.props.modelrunUUID + '/files',
             dataType: 'json',
             cache: false,
             success: function(data) {
@@ -16,7 +20,9 @@ var FileBox = React.createClass({
     },
 
     getInitialState: function() {
-        return {data: []};
+        return {
+            data: {files: []}
+        };
     },
 
     componentDidMount: function() {
@@ -28,9 +34,52 @@ var FileBox = React.createClass({
         return (
             <div className="fileBox">
                 <h1>Files</h1>
+                <FileList data={this.state.data} />
             </div>
         )
     }
 });
+
+
+/**
+ * File list contained within the box
+ */
+var FileList = React.createClass({
+
+    render: function() {
+        console.log(this.props.data.files);
+        var tableRows = this.props.data.files.map(function(file) {
+
+            return (
+                <tr key={file.uuid}>
+                    <td>{file.name}</td>
+                    <td>{file.last_modified}</td>
+                    <td className="download-link">
+                        <a href={file.url}>Download</a>
+                    </td>
+                </tr>
+            );
+        });
+
+        return (
+
+            <div className="fileList">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <td><b>File Name</b></td>
+                            <td><b>Last Modified</b></td>
+                            <td className="download-link"><b>Download Link</b></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+});
+
 
 window.FileBox = FileBox;
