ec34a06177fd564ce3362b44007c08650e9238e5
josepainumkal <josepainumkal@gmail.com>
2016-07-19 16:03:08 -0700

    .

--- app/toolset/views.py
+++ app/toolset/views.py
@@ -196,14 +196,15 @@ def prms_convert():
 
         if fDataExtension.lower() == '.data' and fParamExtension.lower() == '.param' and fControlExtension.lower() == '.control' and fLocationExtension.lower() =='.dat' :
             # saving all the files in UPLOAD_FOLDER
-            try:       
-                inputDataFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputDataFileName))
-                inputParamFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputParamFileName))
-                inputLocationFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputLocationFileName))
-                #inputControlFile.save(os.path.join(app.config['DOWNLOAD_FOLDER'], inputControlFileName))
-                # Since control file need not be converted to netcdf,copying control file directly to download folder
-                inputControlFile.save(os.path.join(app.config['DOWNLOAD_FOLDER'], inputControlFileName))
-
+                   
+            inputDataFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputDataFileName))
+            inputParamFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputParamFileName))
+            inputLocationFile.save(os.path.join(app.config['UPLOAD_FOLDER'], inputLocationFileName))
+            #inputControlFile.save(os.path.join(app.config['DOWNLOAD_FOLDER'], inputControlFileName))
+            # Since control file need not be converted to netcdf,copying control file directly to download folder
+            inputControlFile.save(os.path.join(app.config['DOWNLOAD_FOLDER'], inputControlFileName))
+            
+            try:
                 nhrucells = int(rows) * int(cols)
                 dataToNetcdf.data_to_netcdf(os.path.join(app.config['UPLOAD_FOLDER'], inputDataFileName),os.path.join(app.config['DOWNLOAD_FOLDER'], outputDataFileName))
                 parameterToNetcdf.parameter_to_netcdf(os.path.join(app.config['UPLOAD_FOLDER'], inputParamFileName), os.path.join(app.config['UPLOAD_FOLDER'], inputLocationFileName), nhrucells, int(rows), int(cols), os.path.join(app.config['DOWNLOAD_FOLDER'], outputParamFileName))
